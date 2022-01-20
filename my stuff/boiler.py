a43d325a16594639e3db39528836cde98b14e3ad
EanNewton <eannewton5@gmail.com>
2022-01-19 17:06:15 -0700

    Update boiler.py

--- my stuff/boiler.py
+++ my stuff/boiler.py
@@ -8,8 +8,58 @@ __version__ = "0.1.0"
 __license__ = "AGPL-3.0"
 
 import argparse
+from contextlib import contextmanager
+from subprocess import Popen
+from os import getpid
+from signal import SIGINT
+from time import sleep, time
+from resource import getrusage, RUSAGE_SELF
+
 
 from logzero import logger
+
+
+DEFAULT_DIR = path.dirname(path.abspath(__file__))
+VERBOSE = 1
+events = [
+    "instructions",
+    "cache-references",
+    "cache-misses",
+    "avx_insts.all",
+]
+
+
+@contextmanager
+def perf():
+    """Benchmark this process with Linux's perf util.
+
+    Example usage:
+
+        with perf():
+            x = run_some_code()
+            more_code(x)
+            all_this_code_will_be_measured()
+    """
+    p = Popen([
+        # Run perf stat
+        "perf", "stat",
+        # for the current Python process
+        "-p", str(getpid()),
+        # record the list of events mentioned above
+        "-e", ",".join(events)])
+    # Ensure perf has started before running more
+    # Python code. This will add ~0.1 to the elapsed
+    # time reported by perf, so we also track elapsed
+    # time separately.
+    sleep(0.1)
+    start = time()
+    try:
+        yield
+    finally:
+        print(f"Elapsed (seconds): {time() - start}")
+        print("Peak memory (MiB):",
+              int(getrusage(RUSAGE_SELF).ru_maxrss / 1024))
+        p.send_signal(SIGINT)
 
 
 def main(args):
@@ -17,6 +67,7 @@ def main(args):
     logger.info("hello world")
     logger.info(args)
 
+    
 
 def wikisearch(query: str) -> str:
     """
@@ -46,6 +97,32 @@ def debug(func):
         return wrapper_debug
     else:
         return func
+    
+    
+def timer(func):
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        tic = time.perf_counter()
+        value = func(*args, **kwargs)
+        toc = time.perf_counter()
+        elapsed_time = toc - tic
+        print(f"Elapsed time: {elapsed_time:0.4f} seconds")
+        return value
+    return wrapper_timer
+
+
+def multi_in() -> list:
+    """
+    Take input containing line breaks as single input.
+    :return: list
+    """
+    lines = []
+    while True:
+        line = input()
+        if line:
+            lines.append(line)
+        else: break
+    return lines
 
 
 def fetch_file(directory, filename):
@@ -58,6 +135,21 @@ def wrap(s, w):
     """Break a long string s into a list of strings of length w"""
     return [s[i:i + w] for i in range(0, len(s), w)]
 
+
+def matrix_rotate(raw: list) -> list:
+    """Rotate elements in a list of lists by 90 degrees, ie columns -> rows"""
+    return [[_[each] for _ in raw] for each in range(len(raw[0]))]
+
+
+def flatten_lists(list_of_lists: list) -> list:
+    return [item for sublist in list_of_lists for item in sublist]
+
+
+if __name__ == '__main__':
+    print(globals()['d' + input('> ')](input('>> ')))  # ENTER to EOF
+    # print(globals()['d' + input('>')](multi_in()))  # ENTER to EOF
+    # print(globals()['d'+input('>')](sys.stdin.readlines())) # Ctrl-D to EOF
+    # print(globals()['d' + input('>')](open(0).read()))  # Ctrl-D to EOF
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
